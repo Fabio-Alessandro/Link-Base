ed8c4e5f8065e153ffcba1663af477df34e4a8fa
Fabio-Alessandro <fabioalessandrotr@gmail.com>
2023-04-21 01:36:35 -0600

    Made commands clearer, fined tuned bash scripts to be as precise and informtive as possible.

--- client-setup.sh
+++ client-setup.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 cd Back-End
 
-echo "Seeding database..."
+echo "${BLUE}Seeding database...${NC}"
 
 npm run seed
 
-echo "Done!"
+echo "${GREEN}Done!${NC}"
 
 cd ../Front-End
 
-echo "Initializing React Native development server..."
+echo "${BLUE}Creating Front-End environment variables...${NC}"
+
+read -p "Enter your IP address (don't include http:// or port #): " IP_ADDRESS
+
+echo "IP_ADDRESS=http://$IP_ADDRESS:3001" > ./.env
+echo "${GREEN}Done!${NC}"
+
+echo "${BLUE}Installing Front-End dependencies${NC}"
+
+npm i
+
+echo "${GREEN}Done!${NC}"
 
 npx expo start
--- server-setup.sh
+++ server-setup.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 createdb linkbase
 
-cd Front-End
+cd Back-End
 
-echo "Creating Front-End environment variables..."
+echo "${BLUE}Creating Back-End environment variables...${NC}"
 
-read -p "Enter your IP address (don't include http:// or port): " IP_ADDRESS
+read -p "Enter your PostgreSQL username (press enter if null): " DB_USERNAME
+read -p "Enter your PostgreSQL password (press enter if null): " DB_PASSWORD
 
-echo "IP_ADDRESS=http://$IP_ADDRESS:3001" > ../Front-End/.env
-echo "Done!"
-
-npm i
-
-cd ../Back-End
-
-echo "Creating Back-End environment variables..."
-
-read -p "Enter your PostgreSQL username (or enter if null): " DB_USERNAME
-read -p "Enter your PostgreSQL password (or enter if null): " DB_PASSWORD
-
-echo "PORT=3001" > ../Back-End/.env
-echo "DB_USERNAME=\"$DB_USERNAME\"" >> ../Back-End/.env
-echo "DB_PASSWORD=\"$DB_PASSWORD\"" >> ../Back-End/.env
-echo "DB_NAME=linkbase" >> ../Back-End/.env
-echo "SECRET=secret" >> ../Back-End/.env
-echo "Done!"
+echo "PORT=3001" > ./.env
+echo "DB_USERNAME=\"$DB_USERNAME\"" >> ./.env
+echo "DB_PASSWORD=\"$DB_PASSWORD\"" >> ./.env
+echo "DB_NAME=linkbase" >> ./.env
+echo "SECRET=secret" >> ./.env
+echo "${GREEN}Done!${NC}"
+echo "${BLUE}Installing Back-End dependencies${NC}"
 
 npm i
 
-echo "Initializing server..."
+echo "${GREEN}Done!${NC}"
 
 npm start
